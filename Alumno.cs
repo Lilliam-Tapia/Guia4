b3ba77f720f31d1d3096d88cbe08ff13a86b3fdb
Lilliam-Tapia <liliamtapia377@gmail.com>
2024-05-12 20:14:49 -0600

    se creo Guia4

---
+++ Alumno.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia4
+{
+    public partial class Alumno
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string carne { get; set; }
+        public Carreras carrera { get; set; }
+        public List<double> Calificaciones { get; set; }
+
+        public Alumno(string nombre, int edad, Carreras carrera, string carne)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            this.carrera = carrera;
+            this.carne = carne;
+            this.Calificaciones = new List<double>();
+        }
+        public double CalcularPromedioCalificaciones()
+        {
+            if (Calificaciones.Count == 0)
+            {
+                return 0;
+            }
+            double sumaCalificaciones = 0;
+            foreach (var calificacion in Calificaciones)
+            {
+                sumaCalificaciones += calificacion;
+            }
+            return sumaCalificaciones / Calificaciones.Count;
+        }
+        public string EstadoAprobacion()
+        {
+            double promedo = CalcularPromedioCalificaciones();
+            if (promedo >= 60)
+            {
+                return "Aprobado";
+            }
+            else
+            {
+                return "Reprobado";
+            }
+        }
+        public enum Carreras
+        {
+            Ingenieria_de_Sistemas,
+            Ingenieria_en_computacion,
+            Telecomunicaciones,
+            Ingeneiria_electronica
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Universidad universidad = new Universidad();
+           
+            Alumno alumno1 = new Alumno("María", 18, Alumno.Carreras.Ingenieria_de_Sistemas, "2023-0910U");
+            Alumno alumno2 = new Alumno("José", 20, Alumno.Carreras.Telecomunicaciones, "2021-0916U");
+            alumno1.Calificaciones.Add(80);
+            alumno1.Calificaciones.Add(89);
+            alumno2.Calificaciones.Add(90);
+            alumno2.Calificaciones.Add(70);
+
+            universidad.AgregarAlumno(alumno1);
+            universidad.AgregarAlumno(alumno2);
+
+            List<Alumno> alumnosUni = universidad.ObtenerListaAlumnos();
+            
+            foreach(Alumno alumno in alumnosUni)
+            {
+                Console.WriteLine($"Nombre:{alumno.Nombre}");
+                Console.WriteLine($"Edad: { alumno.Edad}");
+                Console.WriteLine($"Número de carné: {alumno.carne}");
+                Console.WriteLine($"Calificaciones: {alumno.CalcularPromedioCalificaciones()}");
+                Console.WriteLine($"Estado de aprobación: {alumno.EstadoAprobacion() }");
+
+
+            }
+         }
+    }
+}
